b859933c1ebc31ad0e1eed9c47d3d765000dda81
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-09 17:04:49 +0200

    feat: added the TrainsTableSeeder

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Train;
 use Illuminate\Database\Seeder;
 
 class TrainsTableSeeder extends Seeder
@@ -28,14 +29,62 @@ class TrainsTableSeeder extends Seeder
                 'company' => "trenitalia",
                 'departure_station' => "milazzo",
                 'arrival_station' => "milano",
-                'departure_time' => "10:00",
-                'arrival_time' => "trenitalia",
-                'train_code' => "trenitalia",
-                'number_of_carriages' => "trenitalia",
-                'is_in_time' => "trenitalia",
-                'is_deleted' => "trenitalia",
+                'departure_time' => '2023/09/09 21:00',
+                'arrival_time' => '2023/09/09 23:00',
+                'train_code' => "ad345",
+                'number_of_carriages' => 13,
+                'is_in_time' => true,
+                'is_deleted' => true,
             ],
+            [
+                'company' => "trenitalia",
+                'departure_station' => "milazzo",
+                'arrival_station' => "milano",
+                'departure_time' => '2022/09/09 21:00',
+                'arrival_time' => '2022/09/09 23:00',
+                'train_code' => "ad345",
+                'number_of_carriages' => 13,
+                'is_in_time' => true,
+                'is_deleted' => true,
+            ],
+            [
+                'company' => "trenitalia",
+                'departure_station' => "milazzo",
+                'arrival_station' => "milano",
+                'departure_time' => '2021/09/09 21:00',
+                'arrival_time' => '2021/09/09 23:00',
+                'train_code' => "ad345",
+                'number_of_carriages' => 13,
+                'is_in_time' => true,
+                'is_deleted' => true,
+            ],
+            [
+                'company' => "trenitalia",
+                'departure_station' => "milazzo",
+                'arrival_station' => "milano",
+                'departure_time' => '2020/09/09 21:00',
+                'arrival_time' => '2020/09/09 23:00',
+                'train_code' => "ad345",
+                'number_of_carriages' => 13,
+                'is_in_time' => true,
+                'is_deleted' => true,
+            ],
+        ];
 
-        ]
+        foreach($trains as $train){
+            $newTrain = new Train();
+            $newTrain->company = $train['company'];
+            $newTrain->departure_station = $train['departure_station'];
+            $newTrain->arrival_station = $train['arrival_station'];
+            $newTrain->departure_time = $train['departure_time'];
+            $newTrain->arrival_time = $train['arrival_time'];
+            $newTrain->train_code = $train['train_code'];
+            $newTrain->number_of_carriages = $train['number_of_carriages'];
+            $newTrain->is_in_time = $train['is_in_time'];
+            $newTrain->is_deleted = $train['is_deleted'];
+            $newTrain->save();
+        }
+
+
     }
 }
