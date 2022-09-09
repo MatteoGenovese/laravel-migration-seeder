b17d2ea71dc699c5a1b9ef3d6e234fb3dbed00f1
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-09 17:23:22 +0200

    submit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,54 @@
 
 @section('main-content')
 
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="container">
+        <h1>In partenza oggi</h1>
+        <div class="row">
+            @forelse ($trains as $train)
+
+                @if ($train->departure_time==date("Y-m-d"))
+                        <div class="card col-3" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $train->departure_station }} - {{ $train->arrival_station }}</h5>
+                                <p class="card-text">{{ $train->company }}</p>
+                                <p class="card-text">{{ $train->is_in_time }}</p>
+                                <p class="card-text">Data di partenza: {{ $train->departure_time }}</p>
+                            </div>
+                        </div>
+                @endif
+                @empty
+
+            @endforelse
         </div>
     </div>
+
+
+
+    <div class="container">
+        <h1>In partenza nei prossimi giorni o già partiti</h1>
+        <div class="row">
+    @forelse ($trains as $train)
+
+            @if ($train->departure_time!=date("Y-m-d"))
+                <div class="card col-3" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $train->departure_station }} - {{ $train->arrival_station }}</h5>
+                        <p class="card-text">{{ $train->company }}</p>
+                        <p class="card-text">{{ $train->is_in_time }}</p>
+                        <p class="card-text">Data di partenza: {{ $train->departure_time }}</p>
+                    </div>
+                </div>
+            @endif
+
+
+            @empty
+
+            @endforelse
+        </div>
+    </div>
+
+
 
 @endsection
